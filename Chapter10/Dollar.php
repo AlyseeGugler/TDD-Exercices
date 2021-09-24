a2c18a246573540679dd02168efcc6c01aeb46b1
algugler <al.gugler@mob.ch>
2021-09-24 15:30:52 +0200

    [DONE] Initial files for Chaper 10

---
+++ Chapter10/Dollar.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Chapter10;
+/**
+ * Author: algugler
+ * Date de création: 17.09.2021
+ * Description:
+ **/
+class Dollar extends Money
+{
+    public function __construct($amount, string $currency){
+        parent::__construct($amount,$currency);
+    }
+    public function times(int $multiplier) :Money{
+        return Money::dollar($this->amount * $multiplier);
+    }
+
+    public function currency() :string{
+        return $this->currency;
+    }
+}
---
+++ Chapter10/Money.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Author: algugler
+ * Date de création: 24.09.2021
+ * Description:
+ **/
+
+namespace App\Chapter10;
+
+abstract class Money
+{
+    protected $amount;
+    protected $currency;
+
+    public function __construct(int $amount, string $currency) {
+        $this->amount = $amount;
+        $this->currency = $currency;
+    }
+
+    public function equals(object $money) :bool {
+        return $this->amount == $money->amount && get_class($this)==get_class($money);
+    }
+    static function dollar(int $amount) :Money {
+        return new Dollar($amount, "USD");
+    }
+
+    static function franc(int $amount) :Money {
+        return new Franc($amount, "CHF");
+    }
+
+    abstract function times(int $multiplier) :Money;
+
+    public function currency() :string {
+        return $this->currency;
+    }
+}
